bcc1bbb7db1469b09b8b675a3edbcd94520c246d
Doreen Mensah <doreenradiance@gmail.com>
2021-01-14 09:19:41 +0000

    Imported CreateStackNavigation and Navigation from it's DOCS

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FirstScreen from './src/screens/FirstScreen';
+import GetStartedScreen from './src/screens/GetStartedScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          // title: "GetStarted",
+          // headerStyle:{
+          //   backgroundColor:"#3b31eb"
+          // },
+          headerTintColor: "#3b31eb",
+          headerTitleAlign: "center",
+          // headerRight: () => (<Text style={{ color: "#3b31eb", marginRight: 20 }}>Icons</Text>
+          // )
+        }}
+      >
+        <Stack.Screen
+          options={{
+            header:()=>null
+          }}
+          name='GetStarted' component={GetStartedScreen} />
+        <Stack.Screen name='GetStartedScreen' component={GetStartedScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <FirstScreen />
+//       {/* <GetStartedScreen/> */}
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // backgroundColor: '#fff',
+//     // alignItems: 'center',
+//     // justifyContent: 'center',
+//   },
+// });
