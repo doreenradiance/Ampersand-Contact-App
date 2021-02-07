55ef9d9710e33016065541f16a87b7f12feda266
Doreen Mensah <doreenradiance@gmail.com>
2021-02-07 21:54:19 +0000

    Created a case for logged_in and logged_out.

--- src/redux/reducers/authReducers.js
+++ src/redux/reducers/authReducers.js
@@ -1,14 +1,14 @@
-const initialStaten= {
+const initialState= {
 login: false,
 user: null
 }
 
-export default reducer = (state, action) => {
+export default reducer = (state=initialState, action) => {
     switch(action.type){
         case "LOGGED_IN":
-        return {login:true, user:action.payload}
+        return {...state, login:true, user:action.payload}
         case "LOGGED_OUT":
-            return {login:false, user:null}
+            return {...state, login:false, user:null}
     default:
         return state;
     }
