1774262910ccc7eb610e1095187e0815c164a56b
Doreen Mensah <doreenradiance@gmail.com>
2021-02-07 16:48:52 +0000

    Changed the screen that needs to be naviagated

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -87,7 +87,7 @@ function RegisterScreen({ route, navigation }) {
             <View style={{ backgroundColor: "#e6e7e8", height: 2, width: 330, marginBottom: 20, marginLeft: 15 }}></View>
 
             <View style={styles.footer}>
-                <TouchableOpacity onPress={() => { navigation.navigate("DetailsScreen") }}
+                <TouchableOpacity onPress={() => { navigation.navigate("ProfileDetailScreen") }}
                     style={styles.buttonContainer}>
                     <Text style={styles.buttonText}>REGISTER</Text>
                 </TouchableOpacity>
@@ -137,6 +137,6 @@ const styles = StyleSheet.create({
 
 export default connect(
  function () {
-     return 
+     return {}
  }
 ,{}) (RegisterScreen);
