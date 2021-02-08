18684aeeb85f2b200f4ebbf270608e41956d71a8
Doreen Mensah <doreenradiance@gmail.com>
2021-02-08 10:09:18 +0000

    Wrapped persistGate over the appContainer

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Provider } from "react-redux";
-import store from "./src/redux/store";
+import {Provider} from "react-redux";
+import {store, persistor} from "../src/redux/store";
 import AppContainer from "./src/navigation/navigation";
+import {PersistGate} from "redux-persist/integration/react";
 
 function App() {
   return (
-    <Provider store={store}>
-<AppContainer/>
+    <Provider store={store}> 
+      <PersistGate loading={null} persistor={persistor}>
+        <AppContainer />
+      </PersistGate>
+
     </Provider>
   );
 }
