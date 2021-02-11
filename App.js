14a624ce9fd250af10d033f91c2a129b8c968bb9
Doreen Mensah <doreenradiance@gmail.com>
2021-02-11 11:01:04 +0000

    Change the location of the store and persistor

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from "react-redux";
-import {store, persistor} from "../src/redux/store";
+import {store, persistor} from "./src/redux/store";
 import AppContainer from "./src/navigation/navigation";
 import {PersistGate} from "redux-persist/integration/react";
 
