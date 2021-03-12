0083b505610e0e149131892d31e084e1f397606a
Doreen Mensah <doreenradiance@gmail.com>
2021-03-12 08:23:23 +0000

    Implemented the auth.login ?

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Text } from "react-native";
 import { AntDesign } from '@expo/vector-icons';
 import FirstScreen from '../screens/FirstScreen';
 import GetStartedScreen from '../screens/GetStartedScreen';
@@ -17,124 +17,127 @@ import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator()
 
-function AppContainer({ auth,logout }) {
+function AppContainer({ auth, logout }) {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        // screenOptions={{
-        //   headerTintColor: "#3b31eb",
-        //   headerTitleAlign: "center",
-        //   headerBackTitle: ""
-        // }}
-      >
+      {
+        auth.login ?
+          <Stack.Navigator
+          // screenOptions={{
+          //   headerTintColor: "#3b31eb",
+          //   headerTitleAlign: "center",
+          //   headerBackTitle: ""
+          // }}
+          >
 
-        <Stack.Screen
-          options={{
-            headerRight: () => (
-              <TouchableOpacity style={{marginRight:20}}
-              onPress={logout}>
-                <Text>Logout</Text>
-              </TouchableOpacity>
-            ),
-            title: "My Profile",
-            headerStyle: {
-              backgroundColor: "#de4f45"
-            },
-            headerTintColor: "white",
-            headerTitleAlign: "center",
-          }}
-          name='ProfileDetailScreen' component={ProfileDetailScreen} />
-      </Stack.Navigator>
-:
+            <Stack.Screen
+              options={{
+                headerRight: () => (
+                  <TouchableOpacity style={{ marginRight: 20 }}
+                    onPress={logout}>
+                    <Text>Logout</Text>
+                  </TouchableOpacity>
+                ),
+                title: "My Profile",
+                headerStyle: {
+                  backgroundColor: "#de4f45"
+                },
+                headerTintColor: "white",
+                headerTitleAlign: "center",
+              }}
+              name='ProfileDetailScreen' component={ProfileDetailScreen} />
+          </Stack.Navigator>
+          :
 
-      <Stack.Navigator
-      screenOptions={{
-          headerTintColor: "#3b31eb",
-          headerTitleAlign: "center",
-          headerBackTitle: ""
-        }}
-        >
-      <Stack.Screen
-        options={{
-          header: () => null
-        }}
-        name='FirstScreen' component={FirstScreen} />
+          <Stack.Navigator
+            screenOptions={{
+              headerTintColor: "#3b31eb",
+              headerTitleAlign: "center",
+              headerBackTitle: ""
+            }}
+          >
+            <Stack.Screen
+              options={{
+                header: () => null
+              }}
+              name='FirstScreen' component={FirstScreen} />
 
-      <Stack.Screen
-        options={{
-          // header: () => null
-          title: "",
+            <Stack.Screen
+              options={{
+                // header: () => null
+                title: "",
 
-          headerStyle: {
-            backgroundColor: "white"
-          },
-          headerTintColor: "black",
-          headerTitleAlign: "center",
-        }}
-        name='GetStartedScreen'
-        component={GetStartedScreen}
-      />
+                headerStyle: {
+                  backgroundColor: "white"
+                },
+                headerTintColor: "black",
+                headerTitleAlign: "center",
+              }}
+              name='GetStartedScreen'
+              component={GetStartedScreen}
+            />
 
-      <Stack.Screen
-        options={{
-          title: "SIGN IN",
-          headerStyle: {
-            backgroundColor: "#de4f45"
-          },
-          headerTintColor: "white",
-          headerTitleAlign: "center",
-        }}
-        name='LogInScreen' component={LogInScreen} />
+            <Stack.Screen
+              options={{
+                title: "SIGN IN",
+                headerStyle: {
+                  backgroundColor: "#de4f45"
+                },
+                headerTintColor: "white",
+                headerTitleAlign: "center",
+              }}
+              name='LogInScreen' component={LogInScreen} />
 
-      <Stack.Screen
-        options={{
-          // header: () => null
-          title: "REGISTER",
-          headerStyle: {
-            backgroundColor: "#de4f45"
-          },
-          headerTintColor: "white",
-          headerTitleAlign: "center",
-        }}
-        name='RegisterScreen' component={RegisterScreen} />
+            <Stack.Screen
+              options={{
+                // header: () => null
+                title: "REGISTER",
+                headerStyle: {
+                  backgroundColor: "#de4f45"
+                },
+                headerTintColor: "white",
+                headerTitleAlign: "center",
+              }}
+              name='RegisterScreen' component={RegisterScreen} />
 
-      <Stack.Screen
-        options={({ route, navigation }) => ({
-          title: "CODETRAIN",
-          headerStyle: {
-            backgroundColor: "#de4f45"
-          },
-          headerTintColor: "white",
-          headerTitleAlign: "center",
-          headerRight: () => (<TouchableOpacity onPress={() => {
-            navigation.navigate("ProfileDetailScreen")
-          }} style={{ color: "#3b31eb", marginRight: 20 }}>
-            <AntDesign name="user" size={24} color="white" /></TouchableOpacity>)
+            <Stack.Screen
+              options={({ route, navigation }) => ({
+                title: "CODETRAIN",
+                headerStyle: {
+                  backgroundColor: "#de4f45"
+                },
+                headerTintColor: "white",
+                headerTitleAlign: "center",
+                headerRight: () => (<TouchableOpacity onPress={() => {
+                  navigation.navigate("ProfileDetailScreen")
+                }} style={{ color: "#3b31eb", marginRight: 20 }}>
+                  <AntDesign name="user" size={24} color="white" /></TouchableOpacity>)
 
-        })}
-        name='ScanScreen' component={ScanScreen} />
+              })}
+              name='ScanScreen' component={ScanScreen} />
 
-      <Stack.Screen
-        options={{
-          header: () => null
-        }}
-        name='ScanningScreen' component={ScanningScreen} />
+            <Stack.Screen
+              options={{
+                header: () => null
+              }}
+              name='ScanningScreen' component={ScanningScreen} />
 
-      <Stack.Screen
-        options={{
-          // header: () => null
-          title: "Member Profile",
-          headerStyle: {
-            backgroundColor: "#de4f45"
-          },
-          headerTintColor: "white",
-          headerTitleAlign: "center",
-        }}
-        name='DetailsScreen' component={DetailsScreen} />
+            <Stack.Screen
+              options={{
+                // header: () => null
+                title: "Member Profile",
+                headerStyle: {
+                  backgroundColor: "#de4f45"
+                },
+                headerTintColor: "white",
+                headerTitleAlign: "center",
+              }}
+              name='DetailsScreen' component={DetailsScreen} />
 
 
-      </Stack.Navigator>
+          </Stack.Navigator>
+      }
     </NavigationContainer >
   );
 }
@@ -143,4 +146,4 @@ const mapStateToProp = (state) => {
   return { auth: state }
 }
 
-export default connect(mapStateToProp,{logout})(AppContainer);
+export default connect(mapStateToProp, { logout })(AppContainer);
